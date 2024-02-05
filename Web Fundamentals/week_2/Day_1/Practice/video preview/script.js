d138beed98714124fdc43de4aa41be6ee766c1e6
M-Baa <maryembelhaj2345678@gmail.com>
2024-02-05 22:03:20 +0100

    practice

---
+++ Web Fundamentals/week_2/Day_1/Practice/video preview/script.js
@@ -0,0 +1,13 @@
+var video = document.querySelector("#myVideo");
+
+    var playButton = document.querySelector("#playButton");
+    
+    var pauseButton = document.querySelector("#pauseButton");
+
+    playButton.addEventListener("click", function() {
+        video.play();
+    });
+
+    pauseButton.addEventListener("click", function() {
+        video.pause();
+    });
