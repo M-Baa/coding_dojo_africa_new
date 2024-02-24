12661a600f0152e1b31302b02d4aaa0fef49597c
M-Baa <maryembelhaj2345678@gmail.com>
2024-02-25 00:25:02 +0100

    chaining

---
+++ Python/python-fund/Python/Week 1/Day 2/Practice/chaining_methods/Chaining_Methods.py
@@ -0,0 +1,28 @@
+class user:
+    def __init__(self,first_name,last_name,email,age,is_rewards_members=False,gold_card_points=0):
+        self.first_name=first_name
+        self.last_name=last_name
+        self.email=email
+        self.age=age
+        self.is_rewards_members=is_rewards_members
+        self.gold_card_points=gold_card_points
+    def display_info(self):
+        print(f"first.name:{self.first.name},\n last_name:{self.last_name},\n email:{self.email},\n age:{self.age}is_rewards_members:{self.is_rewards_members},\n gold_card_points:{self.gold_card_points}")
+
+    def enroll(self):
+        self.is_rewards_members=True
+        self.gold_card_points=200
+    
+    def spend_points(self,amount):
+        if amount<self.gold_card_points:
+            self.gold_card_points ==  self.gold_card_points-amount
+            print(f"{amount}points spent.")
+        
+maryem = user ("maryem","belhaj","mary@gmail.com",24)
+iheb = user ("iheb", "mechi","iheb@gmail.com",17)
+maryem.display_info().enroll().spend_points(50).display_info()
+iheb.enroll().spend_points(80).display_info()
+
+
+
+
