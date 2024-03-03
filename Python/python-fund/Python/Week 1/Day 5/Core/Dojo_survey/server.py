390bfcf1677e16e7f33402c24d8dd9f790544b12
Maryem belhaj <102475136+M-Baa@users.noreply.github.com>
2024-03-03 16:04:59 +0100

    server.py

--- Python/python-fund/Python/Week 1/Day 5/Core/Dojo_survey/server.py
+++ Python/python-fund/Python/Week 1/Day 5/Core/Dojo_survey/server.py
@@ -1,6 +1,6 @@
 from flask import Flask , render_template , session , redirect , request
 app = Flask(__name__)
-app.secret_key= 'mlsd;ls;dmsq;lmd'   
+app.secret_key= 'shhhhhhh'   
 
 @app.route("/")
 def dojosurvey():
