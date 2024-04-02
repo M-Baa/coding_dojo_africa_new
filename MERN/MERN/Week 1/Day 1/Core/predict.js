2299f5fc155375093ea57213cf9afdd6e793b9a2
M-Baa <maryembelhaj2345678@gmail.com>
2024-04-02 13:45:17 +0100

    core

---
+++ MERN/MERN/Week 1/Day 1/Core/predict.js
@@ -0,0 +1,25 @@
+// problem 1 will output:
+// tesla 
+// Mercedes
+
+
+
+//problem 2 will output:
+// 
+//
+
+
+//problem 3 will output:
+// 12345
+// undefined
+
+
+//problem 4 will output:
+// false
+// true 
+
+
+//problem 5 will output:
+//[1,5,1,8,3,3]
+//1
+//5
