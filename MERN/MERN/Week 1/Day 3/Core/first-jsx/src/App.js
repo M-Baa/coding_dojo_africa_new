c38ac99fda3d272abb0c65c412ce0d94c16cdda1
M-Baa <maryembelhaj2345678@gmail.com>
2024-04-04 14:43:36 +0100

    core

---
+++ MERN/MERN/Week 1/Day 3/Core/first-jsx/src/App.js
@@ -0,0 +1,19 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div>
+
+    <h1> Hello Dojo</h1>
+    <h3>Things I need to do </h3>
+    <p>* Learn React </p>
+    <p>* Climb Mt. Everest</p>
+    <p>* Feed the dogs</p>
+  
+    </div>
+    
+  );
+}
+
+export default App;
